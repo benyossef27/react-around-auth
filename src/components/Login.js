6ab6996c67a83549759bef2d97dd3ce7230753da
benyossef27 <93250111+benyossef27@users.noreply.github.com>
2022-06-30 19:10:12 +0300

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -14,7 +14,6 @@ export default function Login({ onLoginClick }) {
     onLoginClick(values);
   }
 
-  const navigate = useNavigate();
   return (
     <div className="sign">
       <h2 className="sign__title">Log in</h2>
